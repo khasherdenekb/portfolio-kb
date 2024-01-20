5a32ba1621c8d09116611c3e0c26badd35415643
Khash-Erdene <khasherdeneswe@gmail.com>
2024-01-20 15:37:42 +0800

    fix preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export const metadata: Metadata = {
       },
     ],
   },
+  metadataBase: new URL("https://khasherdene.vercel.app"),
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
